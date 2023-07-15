93c306da92c2a7fd358be1aa6311bfdcdaf44f14
Zifeng Deng <oi_dzf@qq.com>
2023-07-15 12:14:06 +0800

    rm gnupg and sync for each try

--- config/keyring_install.sh
+++ config/keyring_install.sh
@@ -1,25 +1,33 @@
 #!bash
 
 echo "waiting for filesystem..."
-sleep 1s
+sync
+sleep 3s
 
 # 初始化 sudoers
 cp ./config/sudoers /etc/sudoers
 
 # 添加 pacman 清华源
 cp ./config/pacman.conf /etc/pacman.conf
+sync
 sleep 3s
 
 while true; do
-    pacman-key --init && sleep 1s && pacman-key --populate archlinux && sleep 1s && break
+    rm -rf /etc/pacman.d/gnupg \
+    && sync && sleep 3s \
+    && pacman-key --init \
+    && echo "waiting for filesystem..." \
+    && sync && sleep 3s \
+    && pacman-key --populate archlinux && sync && break
+done
+sleep 5s
+
+while true; do
+    pacman -Sy --noconfirm --disable-download-timeout archlinux-keyring && sync && sleep 3s && break
 done
 
 while true; do
-    pacman -Sy --noconfirm --disable-download-timeout archlinux-keyring && sleep 2s && break
-done
-
-while true; do
-    pacman -Sy --noconfirm --disable-download-timeout archlinuxcn-keyring && sleep 2s && break
+    pacman -Sy --noconfirm --disable-download-timeout archlinuxcn-keyring && sync && sleep 3s && break
 done
 
 pacman -Syu --noconfirm --disable-download-timeout
